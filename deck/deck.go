b7c44260a0d8c1b9ea35e1ddb78be59907c6f6dc
Chris Benedict <chrisbdaemon@gmail.com>
2017-02-26 13:20:38 -0700

    Read text in blocks terminated by newline

--- deck/deck.go
+++ deck/deck.go
@@ -1,14 +1,81 @@
 package deck
 
-import "github.com/alanxoc3/concards-go/card"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/alanxoc3/concards-go/card"
+)
 
 // Deck contains a set of cards
 type Deck struct {
 	Cards []card.Card
 }
 
+type block struct {
+	start int
+	end   int
+	lines []string
+}
+
+// Size returns the number of cards in the deck
+func (d *Deck) Size() int {
+	return len(d.Cards)
+}
+
 // Open opens filename and loads cards into new deck
 func Open(filename string) (d *Deck, err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		err = fmt.Errorf("Unable to open deck: %s", err)
+		return
+	}
+
+	currLine := 0
+	scanner := bufio.NewScanner(file)
+	for {
+		eof, b := nextBlock(scanner, currLine)
+		currLine = b.end + 1
+
+		// handle block
+
+		if eof {
+			break
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		err = fmt.Errorf("Error reading file: %s", err)
+	}
+
 	d = &Deck{}
+	return
+}
+
+func nextBlock(scanner *bufio.Scanner, currLine int) (atEOF bool, b block) {
+	currLine++
+	b.start = currLine
+	b.lines = make([]string, 0)
+
+	scanner.Scan()
+	for {
+		t := scanner.Text()
+		t = strings.TrimRight(t, "\t\n\r ")
+
+		if !scanner.Scan() {
+			atEOF = true
+		}
+
+		if t == "" {
+			break
+		} else {
+			b.lines = append(b.lines, t)
+			currLine++
+		}
+	}
+	b.end = currLine - 1
+
 	return
 }
--- deck/deck_test.go
+++ deck/deck_test.go
@@ -1,8 +1,11 @@
 package deck
 
 import (
+	"bufio"
+	"os"
 	"testing"
 
+	"github.com/alanxoc3/concards-go/card"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,5 +13,30 @@ func TestOpen(t *testing.T) {
 	d, err := Open("sample_deck.txt")
 	assert.NotNil(t, d, "returned nil instead of deck")
 	assert.NoError(t, err, "returned error")
-	assert.True(t, len(d.Cards) > 0, "empty deck")
+	assert.Equal(t, 3, len(d.Cards), "empty deck")
+}
+
+func TestSize(t *testing.T) {
+	d := &Deck{}
+	d.Cards = append(d.Cards, card.Card{})
+	assert.Equal(t, 1, d.Size(), "wrong size")
+}
+
+func TestNextBlock(t *testing.T) {
+	f, _ := os.Open("sample_deck.txt")
+	scanner := bufio.NewScanner(f)
+	_, b := nextBlock(scanner, 0)
+	assert.Equal(t, 1, b.end, "bad end line")
+	assert.Equal(t, 1, len(b.lines), "empty block")
+
+	blockCount := 1
+	for {
+		eof, _ := nextBlock(scanner, 0)
+		blockCount++
+		if eof {
+			break
+		}
+	}
+
+	assert.Equal(t, 5, blockCount, "wrong number of blocks")
 }
