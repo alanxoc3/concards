086951865ea03f13b208b734f877be7ec799758b
Alan Morgan <alanxoc3@gmail.com>
2020-09-29 22:23:23 -0600

    [GH-53] Removed AnswerClassification.
    I updated the sm2 algorithm. Removed tests. Updated & simplified the
    "next streak" logic. And removed AnswerClassification from the plugin
    package.
    
    I feel really good about this change. I'm glad the AnswerClassification
    is gone now.

--- internal/meta/outcome_test.go
+++ internal/meta/outcome_test.go
@@ -37,25 +37,3 @@ func TestOutcomeTargetFalse(t *testing.T) {
 	r := meta.NewOutcomeFromStrings("ff000000000000000000000000000000", "2020-01-01T00:00:00Z", "2020-01-01T00:00:00Z", "12", "12", "12", "0")
 	assert.False(t, r.Target())
 }
-
-func TestOutcomeAnswerClassification(t *testing.T) {
-	tests := []struct {
-		expect       meta.AnswerClassification
-		expectStreak int
-		streak       string
-		answer       string
-	}{
-		{meta.YesWasYes, 2, "1", "1"},
-		{meta.YesWasNo, 0, "-1", "1"},
-		{meta.NoWasYes, 0, "1", "0"},
-		{meta.NoWasNo, -2, "-1", "0"},
-		{meta.YesWasYes, 1, "0", "1"},
-		{meta.NoWasNo, -1, "0", "0"},
-	}
-
-	for _, v := range tests {
-		r := meta.NewOutcomeFromStrings("", "", "", "", "", v.streak, v.answer)
-		assert.Equal(t, v.expect, r.AnswerClassification())
-		assert.Equal(t, v.expectStreak, r.PredStreak())
-	}
-}
--- internal/meta/sm2.go
+++ internal/meta/sm2.go
@@ -9,28 +9,27 @@ import (
 // Modified SM2 Algorithm
 // Returns the duration in nanoseconds for when to review the card next.
 func sm2Exec(r Outcome) float64 {
-	ac := r.AnswerClassification()
 	period := 0.0
 	rank := math.Max(1.3, 2.5+.11*float64(r.YesCount())-.29*float64(r.NoCount())+.06*float64(r.Streak()))
 
 	// Next Day Logic
-	if ac == YesWasYes {
-		if r.Streak() == 0 {
-			period += float64(time.Hour * 24)
-		} else {
-			period += float64(time.Hour * 24 * 6)
-		}
+   if r.Target() {
+      if r.Streak() < 0 {
+         period = float64(time.Minute * 5)
+      } else if r.Streak() == 0 {
+         period += float64(time.Hour * 24)
+      } else if r.Streak() > 0 {
+         period += float64(time.Hour * 24 * 6)
+      }
 
-		if r.Streak() >= 2 {
-			for i := 2; i <= r.Streak(); i++ {
-				period *= rank
-			}
-		}
-	} else if ac == YesWasNo {
-		period = float64(time.Minute * 5)
-	} else {
+      if r.Streak() >= 2 {
+         for i := 2; i <= r.Streak(); i++ {
+            period *= rank
+         }
+      }
+   } else {
 		period = float64(time.Minute * 1)
-	}
+   }
 
 	// Add some noise, so everything doesn't get reviewed at the same time.
 	return period * (1 + .1*rand.Float64())
