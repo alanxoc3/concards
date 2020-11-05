aa35e0d95c06abc5830291fb97e5af5e9541b37d
Alan Morgan <alanxoc3@gmail.com>
2020-11-04 23:11:19 -0700

    [GH-78] Changed streak logic.
    The rank has more leverage now. If the streak is negative, 2 extra
    reviews are required soon. If the streak is 0, 1 extra review is
    required soon.

--- internal/meta/sm2.go
+++ internal/meta/sm2.go
@@ -10,26 +10,29 @@ import (
 // Returns the duration in nanoseconds for when to review the card next.
 func sm2Exec(r Outcome) float64 {
 	period := 0.0
+	exponent := 0
 	rank := math.Max(1.3, 2.5+.11*float64(r.YesCount())-.29*float64(r.NoCount())+.06*float64(r.Streak()))
 
 	// Next Day Logic
-   if r.Target() {
-      if r.Streak() < 0 {
-         period = float64(time.Minute * 5)
-      } else if r.Streak() == 0 {
-         period += float64(time.Hour * 24)
-      } else if r.Streak() > 0 {
-         period += float64(time.Hour * 24 * 6)
-      }
+	if r.Target() && r.Streak() < 0 {
+		period = float64(time.Minute * 2)
+	} else if r.Target() {
+		exponent = r.Streak()
+		if r.Streak() == 0 {
+			period = float64(time.Minute * 4)
+		} else if r.Streak() == 1 {
+			period = float64(time.Hour * 4)
+		} else if r.Streak() > 1 {
+			period = float64(time.Hour * 8)
+		}
+	} else {
+		period = float64(time.Minute)
+	}
 
-      if r.Streak() >= 2 {
-         for i := 2; i <= r.Streak(); i++ {
-            period *= rank
-         }
-      }
-   } else {
-		period = float64(time.Minute * 1)
-   }
+	// Multiply by the rank x number of times.
+	for i := 0; i <= exponent; i++ {
+		period *= rank
+	}
 
 	// Add some noise, so everything doesn't get reviewed at the same time.
 	return period * (1 + .1*rand.Float64())
